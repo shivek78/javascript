519610aae288a0605dd704b8f527b6b173d32675
shivek yadav <shivekyadav0786@gmail.com>
2024-06-13 22:15:00 +0530

    deay 3]

--- topics/javasript/variable.js
+++ topics/javasript/variable.js
@@ -1,2 +1,18 @@
+// addtion of two number in javascript//
+var a, b, c, add, div,multi,mod,sub;
+a=20;
+b=30;
+add=a+b;
+sub=a-b;
+div=a/b;
+multi=a*b;
+mod=a%b;
 
-let name1 ="shivek yadav"
+console.log("addtion of two number=",add);
+console.log("substraction of two number=",sub);
+console.log("divition of two number=",div);
+console.log("multiple of two number=",multi);
+console.log("modalus of two number=",mod);
+
+
+// using all aritmatice  operater
