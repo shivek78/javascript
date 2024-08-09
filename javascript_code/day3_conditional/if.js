45f5ed31fb15bef9a13b686290690202caea33fb
shivek yadav <shivekyadav0786@gmail.com>
2024-08-10 01:38:33 +0530

    java script practices

---
+++ javascript_code/day3_conditional/if.js
@@ -0,0 +1,11 @@
+//program for checking your are  eligibil  for vote ot not
+let age =18;
+if( age==18){
+      console.log("you are eligibil for the vote");
+}
+//program for password
+  let password=2030;
+  if(password==2030){
+      console.log("access granted ")
+
+  }
---
+++ javascript_code/day3_conditional/if_else.js
@@ -0,0 +1,48 @@
+//program uing if else
+let age=20;
+let a="you are eliglbil for the vote"  ;// 
+b="you are not eligibil for the vote";
+if(age>=18){
+      console.log("welcome"+a);
+}
+else{
+      console.log(b)
+}
+//programe for password
+let password=2020;
+ let c="password is correct";
+ let d="thankyou";
+ let e="password is worng buddy";
+ if(password=2020){
+      console.log(c+"    "+d);
+ }
+ else{
+      console.log(e);
+ }
+ 
+ 
+ 
+ /*     Suppose we need to assign different grades to students based on their scores.
+
+If a student scores above 90, assign grade A.
+If a student scores above 75, assign grade B.
+If a student scores above 65, assign grade C.*/
+ let marks=70;
+ if( marks>=65)// its check  the satements 70>65 satement is ture
+      {
+      if(marks>=90){
+            console.log("a")
+      }
+else{
+      console.log("b")
+}
+     
+
+ }
+
+ else{ 
+      console.log("c")
+ }
+ 
+ 
+
