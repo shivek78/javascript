80d798839ec8725370657e206250d4184fe18914
shivek yadav <shivekyadav0786@gmail.com>
2024-08-10 18:02:29 +0530

    new problem  code

---
+++ javascript_code/day4/if_else.js
@@ -0,0 +1,39 @@
+// Check if a number is odd or even in JavaScript
+let num=20;
+if(num%2==0)
+      {
+      console.log("even")
+}
+else{
+      console.log( "odd")
+}
+//Find the largest of three number
+let num1 = 40;
+let num2 = 53;
+let num3 = 36;
+console.log("program for finding the three largest number");
+if(num1<num2){
+      if(num2<num3){
+            console.log("num3 is greater")
+
+      }
+      else{
+            console.log("num2 is grater")
+      }
+}
+else{
+      console.log("num1 is greater")
+}
+
+//sort three numbers using if else
+let sort=20;
+let sort1=40;
+let sort2=60;
+ if(sort>sort1){
+      if{}
+
+ }
+ else{
+      console.log( sort1)
+ }
+
