ba4b3c18e685a785b34634271bf1ed56c2f2c015
shivek yadav <shivekyadav0786@gmail.com>
2024-10-09 11:07:22 +0530

    program

---
+++ javascript_code/day4/mode.js
@@ -0,0 +1,18 @@
+// program for the  mode changing in java scripts
+let mode="blue"
+let color;
+ if(mode==="dark")
+{
+      color="black";
+}
+else if(mode==="blue")
+ {
+      color="blue";
+}
+else if(mode==="green")
+{
+color="green";
+}
+console.log(color)
+
+
