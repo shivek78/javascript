d1f635f493dba066c80d857e9ea5d72e74fd443c
shivek78 <ggi2022.1575@ggi.ac.in>
2025-06-09 20:41:38 +0530

    function_grate

--- javascript_code/day7/index.js
+++ javascript_code/day7/index.js
@@ -1,5 +1,7 @@
-console.log("we are learing functions")
-console.log("function is block of code that used to recuced the number of codeline ")
+console.log("we are learing functions");
+console.log(
+      "function is block of code that used to recuced the number of codeline "
+);
 //two part function definztion and function caling
 /*
 function function_name(){
@@ -9,18 +11,26 @@ function function_name(){
  function_name()
  */
 
- function student_attnendance(){
-      console.log('present maam')
+function student_attnendance() {
+      console.log("present maam");
+}
+student_attnendance();
 
- }
-student_attnendance( )
-
- function sum(a,b){
-      console.log(a+b)
-      
- }
- sum(10,20)
- function print_name(){
+function print_name() {
       console.log("shivek");
 }
-print_name()
+print_name();
+//function with parameter
+function sum(a, b) {
+      console.log(a + b);
+}
+sum(10, 20);
+sum(100,200)
+sum(5000,30000)
+
+//web printing name 
+function grate(name){
+      return`hello,${name}
+      `
+}
+console.log(grate("shivek yadav"))
