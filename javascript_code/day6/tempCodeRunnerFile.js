baca9849c2af1fa2be16ce0f986142f764dad6c8
shivek78 <ggi2022.1575@ggi.ac.in>
2025-06-09 20:29:44 +0530

    function

--- javascript_code/day6/tempCodeRunnerFile.js
+++ javascript_code/day6/tempCodeRunnerFile.js
@@ -1,30 +1,20 @@
-/*🚀 Problem: FizzBuzz with a Twist
-Write a JavaScript program that prints numbers from 1 to 50.
-
-If the number is divisible by 3, print "Fizz".
-
-If the number is divisible by 5, print "Buzz".
-
-If the number is divisible by both 3 and 5, print "FizzBuzz".
-
-If the number is even but not divisible by 3 or 5, print "Even Number: X" (replace X with the number).
-
-If the number is odd and not divisible by 3 or 5, print "Odd Number: X".
-*/
-let num;
-console.log("program that prints numbers from 1 to 50.")
-for (num = 1; num <= 50; num++) {
-      if (num % 2 == 1)
-            console.log(num + "  " + "Odd")
-      else if (num % 2 == 0)
-            console.log(num + "  " + "even")
-
-      else if (num % 3 == 0) {
-            console.log(num + "  " + "fizz")
-      }
-      else if (num % 5 == 0) {
-            console.log(num + "" + "buzz")
-      }
 
 }
+console.log("Blast off")
 
+*/
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+console.log("---------------------------------");
+console.log("Factorial:=>");
+console.log("Ask the user to enter a non-negative integer. Use a for loop to calculate and print its factorial. (The factorial of a non-negative integer n, denoted by n!, is the product of all positive integers less or equal to n. For example, 5!=5×4×3×2×1=120. 0!=1).");
+console.log("------------------------------------------");
+
+rl.question("Please enter a non-negative integer: ", (input) => {
+    const num = parseInt(input);
+
+    // Validate if the input is a non-negative integer
+    if (num < 0) {
---
+++ javascript_code/day7/index.js
@@ -0,0 +1,26 @@
+console.log("we are learing functions")
+console.log("function is block of code that used to recuced the number of codeline ")
+//two part function definztion and function caling
+/*
+function function_name(){
+//statmets
+}
+ function calling
+ function_name()
+ */
+
+ function student_attnendance(){
+      console.log('present maam')
+
+ }
+student_attnendance( )
+
+ function sum(a,b){
+      console.log(a+b)
+      
+ }
+ sum(10,20)
+ function print_name(){
+      console.log("shivek");
+}
+print_name()
