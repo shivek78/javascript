d01a8d3fd34547830d8230a50f28ddcaba836f17
shivek78 <ggi2022.1575@ggi.ac.in>
2025-06-17 12:04:11 +0530

    local- global variable

--- javascript_code/day7/fun_glopal_local.js
+++ javascript_code/day7/fun_glopal_local.js
@@ -1,9 +1,19 @@
-//global variabl in javascripts 
-function globalscope(){
-      let localvar='i am golable'
-      console.log(localvar)
+/* hi  now we are  learing the loacal and the  global varivale in javascripts*/
+ const glovar='hi  i am the gloabl variable  '
+  const display =()=>{
+      console.log(glovar)
+      console.log("here we printing the local variable ",locvar)
 
+  }
 
-}
-globalscope()
-/////************************************************************** */
+   const locvar=()=>{
+      let y=20;
+      let x=30;
+      console.log(y+x)
+      console.log ("this sum is the local variable")
+   }
+    display();
+    locvar();
+    console.log("here we printing the global variable ",glovar)
+    console.log("here we printing the local variable ",locvar)
+
