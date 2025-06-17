5eee8a9699e302c12ee0f40ceffd88c49025b7b6
shivek78 <ggi2022.1575@ggi.ac.in>
2025-06-17 12:19:56 +0530

    fixing the issue

--- javascript_code/day7/fun_glopal_local.js
+++ javascript_code/day7/fun_glopal_local.js
@@ -1,19 +1,23 @@
 /* hi  now we are  learing the loacal and the  global varivale in javascripts*/
+ /* hi  now we are  learing the loacal and the  global varivale in javascripts*/
  const glovar='hi  i am the gloabl variable  '
   const display =()=>{
       console.log(glovar)
-      console.log("here we printing the local variable ",locvar)
+      
 
   }
 
    const locvar=()=>{
       let y=20;
       let x=30;
-      console.log(y+x)
+       let sum=x+y;
+      console.log(sum)
       console.log ("this sum is the local variable")
    }
-    display();
     locvar();
+    display();
+   
     console.log("here we printing the global variable ",glovar)
-    console.log("here we printing the local variable ",locvar)
+    console.log("here we printing the local variable ",sum)
+
 
