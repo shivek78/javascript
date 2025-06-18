586c67fdc26b76e4dea37592149be08614c5af81
shivek78 <ggi2022.1575@ggi.ac.in>
2025-06-18 11:52:07 +0530

    ...

---
+++ day9/distrucring.js
@@ -0,0 +1,17 @@
+const person = {
+      name: "shivek",
+      age: 30,
+      income: 20000,
+      workprofile: "helper",
+
+}
+const car={
+      brand_name:"land raover",
+      colour:"black",
+      model:2000,
+      price :2+"cr",
+}
+//distructring
+const{ name,age, income}=person
+
+console.log(workprofile)
---
+++ day9/objects.js
@@ -0,0 +1,15 @@
+console.log('we are leARINTHE THE OBJECTS IN JS')
+ 
+//access methond 
+/*with name or with name.key
+
+
+*/ 
+console.log( "in this we access car object ",car);
+console.log( "in this we access key of car with brand name ",car.brand_name);
+console.log( "in this we access person object ",person);
+console.log( "in this we access person name ",person.name);
+
+// copy the person details using spread oprate 
+const shivek={...person,workprofile:"devloper",branch:"cse"}
+console.log(shivek)
